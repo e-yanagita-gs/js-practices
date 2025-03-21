21f53273e3b0eec70a70dde8639a03ea214d3d07
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-03-21 11:28:14 +0900

    fizzbuzzプログラムの追加

---
+++ 01.fizzbuzz/fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzbuzz(i) {
+  if (i % 15 === 0) {
+    return "FizzBuzz";
+  } else if (i % 3 === 0) {
+    return "Fizz";
+  } else if (i % 5 === 0) {
+    return "Buzz";
+  } else {
+    return i.toString();
+  }
+}
+
+for (let i = 1; i <= 20; i++) {
+  console.log(fizzbuzz(i));
+}
