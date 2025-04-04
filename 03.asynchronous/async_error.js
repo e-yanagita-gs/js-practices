ea32d419821f6a8f75734a1b76418f299f0bc0e5
e-yanagita-gs <e.yanagita@genbasupport.com>
2025-04-04 16:13:16 +0900

    非同期処理箇所の見直し

--- 03.asynchronous/async_error.js
+++ 03.asynchronous/async_error.js
@@ -5,26 +5,24 @@ import { runQuery, allQuery, closeDb } from "./dbOperations.js";
 const db = new sqlite3.Database(":memory:");
 
 async function main() {
+  await runQuery(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+  );
+  console.log("テーブルを作成しました");
   try {
-    await runQuery(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
-    );
-    console.log("テーブルを作成しました");
-    const result = await runQuery(db, "INSERT INTO book (title) VALUES (?)", [
-      "Sample",
-    ]);
+    await runQuery(db, "INSERT INTO book (title) VALUES (?)", ["Sample"]);
   } catch (error) {
     console.error("データの追加に失敗しました:", error.message);
   }
   try {
-    const books = await allQuery(db, "SELECT content FROM books");
+    await allQuery(db, "SELECT content FROM books");
   } catch (error) {
     console.error("データの取得に失敗しました:", error.message);
-  } finally {
-    runQuery(db, "DROP TABLE books");
-    console.log("テーブルを削除しました");
-    closeDb(db);
   }
+  runQuery(db, "DROP TABLE books");
+  console.log("テーブルを削除しました");
+  closeDb(db);
 }
+
 main();
--- 03.asynchronous/async_success.js
+++ 03.asynchronous/async_success.js
@@ -5,26 +5,23 @@ import { runQuery, allQuery, closeDb } from "./dbOperations.js";
 const db = new sqlite3.Database(":memory:");
 
 async function main() {
-  try {
-    await runQuery(
-      db,
-      "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
-    );
-    console.log("テーブルを作成しました");
+  await runQuery(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT NOT NULL UNIQUE)",
+  );
+  console.log("テーブルを作成しました");
 
-    const result = await runQuery(db, "INSERT INTO books (title) VALUES (?)", [
-      "Sample",
-    ]);
-    console.log(`データを追加しました。ID: ${result.lastID}`);
+  const result = await runQuery(db, "INSERT INTO books (title) VALUES (?)", [
+    "Sample",
+  ]);
+  console.log(`データを追加しました。ID: ${result.lastID}`);
 
-    const books = await allQuery(db, "SELECT * FROM books");
-    console.log("取得したデータ:", books);
+  const books = await allQuery(db, "SELECT * FROM books");
+  console.log("取得したデータ:", books);
 
-    await runQuery(db, "DROP TABLE books");
-    console.log("テーブルを削除しました");
-  } finally {
-    closeDb(db);
-  }
+  await runQuery(db, "DROP TABLE books");
+  console.log("テーブルを削除しました");
+  closeDb(db);
 }
 
 main();
